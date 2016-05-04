3a00461b532b2434f72db498ae40d52cc08b9261
David Gago <davidopg@gmail.com>
2016-05-04 16:13:48 +0100

    ssl/tlc Imgur proxy server

--- sd/tp1/srv/imgur/ImgurRestServer.java
+++ sd/tp1/srv/imgur/ImgurRestServer.java
@@ -1,5 +1,8 @@
 package sd.tp1.srv.imgur;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -7,8 +10,13 @@ import java.net.SocketException;
 import java.net.URI;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.security.KeyStore;
+import java.security.SecureRandom;
 import java.util.Enumeration;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
@@ -20,23 +28,41 @@ import sd.tp1.srvREST.AlbumResource;
 
 import com.sun.net.httpserver.HttpServer;
 
-@SuppressWarnings("restriction")
 public class ImgurRestServer {
+	static final File KEYSTORE = new File("./server.jks");
+	static final char[] JKS_PASSWORD = "changeit".toCharArray();
+	static final char[] KEY_PASSWORD = "changeit".toCharArray();
 	
 	@SuppressWarnings("unused")
 	public static void main(String[] args) throws Exception {
 
-		URI baseUri = UriBuilder.fromUri("http://0.0.0.0/").port(8080).path("GalleryServerImgur").build();
+		URI baseUri = UriBuilder.fromUri("https://0.0.0.0/").port(8080).path("GalleryServerImgur").build();
 
 		ResourceConfig config = new ResourceConfig();
 
 		config.register(new ImgurProxy());
 		
-		HttpServer server = JdkHttpServerFactory.createHttpServer(baseUri, config);
+		SSLContext sslContext = SSLContext.getInstance("TLSv1");
+
+		KeyStore keyStore = KeyStore.getInstance("JKS");
+
+		try (InputStream is = new FileInputStream(KEYSTORE)) {
+			keyStore.load(is, JKS_PASSWORD);
+		}
+
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		kmf.init(keyStore, KEY_PASSWORD);
+		
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		tmf.init(keyStore);
+
+		sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+		
+		HttpServer server = JdkHttpServerFactory.createHttpServer(baseUri, config, sslContext);
 		
 		System.err.println("GalleryServer started");
 		String serviceURL = ""+localhostAddress().getCanonicalHostName()+":"+baseUri.getPort();
-		String url = "http://"+serviceURL+ "/GalleryServerImgur";
+		String url = "https://"+serviceURL+ "/GalleryServerImgur";
 		System.out.println(url);
 		Discovery discovery = new MulticastDiscovery();
 		discovery.registerService(new URL(url));
